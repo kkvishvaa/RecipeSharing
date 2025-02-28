75528f8e25e658bed2d687d90a2e90dce2ee9162
kkvishvaa <kkvishvaa8787@gmail.com>
2025-03-01 00:09:23 +0530

    mongodbatlas

--- backend/server.js
+++ backend/server.js
@@ -4,15 +4,16 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const authRoutes = require("./routes/authRoutes");
 const recipeRoutes = require("./routes/recipeRoutes");
-
 dotenv.config();
+
+const dbURI = process.env.MONGODB_URL;
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use("/uploads", express.static("uploads"));
 
-mongoose.connect("mongodb://127.0.0.1:27017/authDB", { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
 
