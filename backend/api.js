cfbe5ebc380d045fe93ef9d8339b1c0c705d3db5
kkvishvaa <kkvishvaa8787@gmail.com>
2025-03-01 01:45:21 +0530

    vercel

--- backend/api.js
+++ backend/api.js
@@ -1,4 +1,4 @@
-const app = require("./server"); // Import Express app
+const app = require("./api/server"); // Import Express app
 const serverless = require("serverless-http");
 
 module.exports = serverless(app);
